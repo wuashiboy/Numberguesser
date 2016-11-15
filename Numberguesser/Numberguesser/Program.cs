6e99b06faba9da548a5cd78fd40035aab2071b74
Peter Le <Peter Le>
2016-11-15 18:47:04 -0500

    commite again

--- Numberguesser/Numberguesser/Program.cs
+++ Numberguesser/Numberguesser/Program.cs
@@ -10,6 +10,46 @@ namespace Numberguesser
     {
         static void Main(string[] args)
         {
+
+            {
+                Random num = new Random();
+                var raso = num.Next(100);
+
+                Console.WriteLine(raso);
+
+                Console.WriteLine("Please enter a number between 1 and 100");
+
+                var counter = 0;
+                var win = false;
+
+                while (counter < 5 && !win)
+                {
+                    var input = int.Parse(Console.ReadLine());
+                    
+                    if (input < raso)
+                    {
+                        Console.WriteLine("Sorry you are low, please try again");
+                    }
+                    else if (input > raso)
+                    {
+                        Console.WriteLine("sorry you are high, please try again");
+                    }
+                    else if (input == raso)
+                    {
+                        Console.WriteLine("Winner Winner...Chicken dinner!");
+                        win = true;
+                    }
+                    
+                    counter++;
+                }
+                if (counter == 5)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Sorry, You lost");
+                }
+
+                Console.ReadLine();
+           } 
         }
     }
 }
